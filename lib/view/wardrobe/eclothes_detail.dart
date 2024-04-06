e95ee1c425c9faf6b4cceca1a2b0f5aa8a0942bb
Somix <somix.land@null.net>
2024-04-06 17:22:06 +0200

    detail page mock

---
+++ lib/view/wardrobe/eclothes_detail.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:nfc_app_prototype/models/eclothes.dart';
+
+class EclothesDetailPage extends StatelessWidget {
+  final EClothes eclothes;
+
+  EclothesDetailPage(this.eclothes);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView(
+        children: [
+          Text(eclothes.title, style: const TextStyle(color: Colors.teal, fontSize: 24, fontWeight: FontWeight.bold)),
+          Container(
+            height: 300,
+            width: 200,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              image: DecorationImage(image: AssetImage(eclothes.imageUrl), fit: BoxFit.fitWidth)
+            ),
+          ),
+          Text(eclothes.id),
+          Text(eclothes.category)
+        ],
+      )
+    );
+  }
+}
--- lib/widgets/eclothes_item.dart
+++ lib/widgets/eclothes_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nfc_app_prototype/models/eclothes.dart';
+import 'package:nfc_app_prototype/view/wardrobe/eclothes_detail.dart';
 
 class EClothesItem extends StatelessWidget {
 
@@ -10,40 +11,49 @@ class EClothesItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        margin: const EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            // --- IMAGE ---
-            Container(
-              margin: const EdgeInsets.all(10),
-              height: 170,
-              width: 200,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                image: DecorationImage(image: AssetImage(eclothes.imageUrl), fit: BoxFit.fitWidth)
-              ),
-            ),
-            // --- TITLE ---
-            Text(
-              eclothes.title,
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.teal,
-              ),
-            ),
-            // --- CATEGORY ---
-            Text(
-              eclothes.category,
-              style: const TextStyle(
-                fontWeight: FontWeight.w100
-              ),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => EclothesDetailPage(eclothes)
             )
-          ],
+          );
+        },
+        child: Card(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          margin: const EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // --- IMAGE ---
+              Container(
+                margin: const EdgeInsets.all(10),
+                height: 170,
+                width: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  image: DecorationImage(image: AssetImage(eclothes.imageUrl), fit: BoxFit.fitWidth)
+                ),
+              ),
+              // --- TITLE ---
+              Text(
+                eclothes.title,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal,
+                ),
+              ),
+              // --- CATEGORY ---
+              Text(
+                eclothes.category,
+                style: const TextStyle(
+                  fontWeight: FontWeight.w100
+                ),
+              )
+            ],
+          ),
         ),
-      ),
+      )
     );
   }
 }
